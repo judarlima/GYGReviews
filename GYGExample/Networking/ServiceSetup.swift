890f3e82907ee65cc7dda8549773901e8a1a688c
Judar Lima <judar.lima@ibm.com>
2019-02-23 12:08:19 -0300

    Implements service setup protocol.

---
+++ GYGExample/Networking/ServiceSetup.swift
@@ -0,0 +1,13 @@
+//
+//  ServiceSetup.swift
+//  GYGExample
+//
+//  Created by Judar Lima on 23/02/19.
+//  Copyright © 2019 raduJ. All rights reserved.
+//
+
+import Foundation
+
+protocol ServiceSetup {
+    var endpoint: String { get }
+}
