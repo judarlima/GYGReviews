f87f02d5639f1276cb515b80d49415822abbdfd1
Judar Lima <judar.lima@ibm.com>
2019-02-23 12:12:26 -0300

    Creates API Constants.

---
+++ GYGExample/Common/APIConstants.swift
@@ -0,0 +1,20 @@
+//
+//  APIConstants.swift
+//  GYGExample
+//
+//  Created by Judar Lima on 23/02/19.
+//  Copyright © 2019 raduJ. All rights reserved.
+//
+
+import Foundation
+
+struct APIConstants {
+    static let baseUrl = "https://www.getyourguide.com"
+}
+
+extension APIConstants {
+    struct Path {
+        static let berlinAirliftReviews = "/berlin-l17/tempelhof-2-hour-airport-history-" +
+        "tour-berlin-airlift-more-t23776/reviews.json"
+    }
+}
