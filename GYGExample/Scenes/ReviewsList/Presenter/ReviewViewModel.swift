3e4c48e51e53f5f4d315312b3018d156a428418b
Judar Lima <judar.lima@ibm.com>
2019-02-23 18:52:26 -0300

    Review View Model
    Implements the Review View Model to abstract the business model data and avoid expose these informations through the viewController.

---
+++ GYGExample/Scenes/ReviewsList/Presenter/ReviewViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ReviewViewModel.swift
+//  GYGExample
+//
+//  Created by Judar Lima on 23/02/19.
+//  Copyright © 2019 raduJ. All rights reserved.
+//
+
+import Foundation
+
+struct ReviewViewModel {
+    let title: String
+    let author: String
+    let rating: Double
+    let message: String
+}
