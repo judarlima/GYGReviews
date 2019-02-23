65f923a2d63c4413670ba41fcc43262ed3b43330
Judar Lima <judar.lima@ibm.com>
2019-02-23 12:09:29 -0300

    Creates Initial Reviews View Controller.

---
+++ GYGExample/Scenes/Review/Views/ReviewsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewsViewController.swift
+//  GYGExample
+//
+//  Created by Judar Lima on 23/02/19.
+//  Copyright © 2019 raduJ. All rights reserved.
+//
+
+import UIKit
+
+class ReviewsViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
